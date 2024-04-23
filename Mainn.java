dfe300fad5bb95eb638a418fadd2eac245d14b76
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-04-23 23:24:23 +0530

    23-04-2024

---
+++ Mainn.java
@@ -0,0 +1,11 @@
+public class Mainn {
+    public static void main(String[ ] args) {
+      try {
+        int[] myNumbers = {1, 2, 3};
+        System.out.println(myNumbers[1]);
+      } catch (Exception e) {
+        System.out.println("Something went wrong.");
+      }
+    }
+  }
+
