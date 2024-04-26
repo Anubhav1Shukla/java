8d17f860c944ef28becd3335edb699101472d5d8
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-04-26 20:35:52 +0530

    Age-validator

---
+++ AgeValidator.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class AgeValidator {
+    public static void main(String[] args){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the age: ");
+            String input=sc.nextLine();
+            try{
+                int age=Integer.parseInt(input);
+                if(age<0){
+                    System.out.println("Error: Invalid input");
+                } else{
+                    System.out.println("Your age is: "+age);
+                }
+            } catch(ArithmeticException e){
+                System.out.println("Error: Invalid age input");
+            }
+    }
+}
