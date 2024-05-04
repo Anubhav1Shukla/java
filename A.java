11e9dd45e2d92fe12bb2143f69f3dca21f5d58d2
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-05-04 13:37:29 +0530

    Inheritence

---
+++ A.java
@@ -0,0 +1,31 @@
+class A {
+    void showA() {
+        System.out.println("In class A.");
+    }
+}
+
+class B extends A {
+    void showB() {
+        System.out.println("In class B.");
+    }
+}
+
+class C extends B {
+    void showC() {
+        System.out.println("In class C.");
+    }
+
+    public static void main(String[] args) {
+        A sA = new A();
+        sA.showA();
+
+        B sB = new B();
+        sB.showA();
+        sB.showB();
+
+        C sC = new C();
+        sC.showA();
+        sC.showB();
+        sC.showC();
+    }
+}
---
+++ C.java
@@ -0,0 +1,17 @@
+class A {
+    void showA(){
+        System.out.println("In a class A.");
+    }
+}
+public class B extends A{
+        void showB(){
+            System.out.println("In a class B.");
+        }
+    
+}
+public class  C extends B {
+            void showC(){
+                System.out.println("In a class C.");
+            }
+    
+}
