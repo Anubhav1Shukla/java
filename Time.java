19abbecd31801ea5fa56a7495ba9d42f4ae7897b
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-05-06 09:31:58 +0530

    Time-java

---
+++ Time.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime;
+
+public class Time {
+    public static void main(String[] args){
+        LocalTime LT=LocalTime.now();
+        System.out.println("This current time is: "+LT);
+    }
+}
