1b8f27d4f9353a07e4466f7e6e7d174930636191
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-05-06 09:40:57 +0530

    Date-Time-Formatter

---
+++ DTF.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime; 
+import java.time.format.DateTimeFormatter; 
+
+public class DTF {
+  public static void main(String[] args) {
+    LocalDateTime myDateObj = LocalDateTime.now();
+    System.out.println("Before formatting: " + myDateObj);
+    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    String formattedDate = myDateObj.format(myFormatObj);
+    System.out.println("After formatting: " + formattedDate);
+  }
+}
---
+++ LocalDateTim.java
@@ -0,0 +1,9 @@
+
+import java.time.LocalDateTime;
+
+public class LocalDateTim{
+    public static void main(String[] args){
+        LocalDateTime LDT=LocalDateTime.now();
+        System.out.println("This is localdatetime: "+LDT);
+    }
+}
