1544c6089c684ccc2661c80e527f75d694fd2427
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-05-06 10:22:02 +0530

    Read-in- File

--- ReadFile.java
+++ ReadFile.java
@@ -7,7 +7,16 @@ import java.util.Scanner;
 public class ReadFile {
     public static void main(String[] args) {
         try{
-            FileRead fr
+            File fr=new File("anubhav.txt");
+            Scanner myReader=new Scanner(fr);
+            while (myReader.hasNextLine()) {
+                String data=myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        }catch(FileNotFoundException e){
+            System.out.println("Something Wrong...");
+            e.printStackTrace();
         }
     }
 }
