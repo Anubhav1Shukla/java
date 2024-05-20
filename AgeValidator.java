88a86e98cf5fad30ca45d55228ec79bd055d3320
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-05-21 02:16:43 +0530

    Update AgeValidator.java

--- AgeValidator.java
+++ AgeValidator.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class AgeValidat{
+public class AgeValidator {
     public static void main(String[] args){
             Scanner sc=new Scanner(System.in);
             System.out.println("Enter the age: ");
